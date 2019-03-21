32c8adeec86bffafe68998eece63b70773bd3953
Zhenyu Yang <zhenyuyang@ucsb.edu>
2019-03-21 00:49:27 -0700

    change show()

--- test.cpp
+++ test.cpp
@@ -35,13 +35,19 @@ class Board
 
         void show()
         {
-
             cout<<"------------------"<<endl;
             for(int i = 0 ; i < n ; i++)
             {
                 for(int j = 0 ; j < n ; j++)
                 {
-                    cout<<"["<<data[i][j]<<"]";
+                    if(data[i][j]==0)
+                    {
+                        cout<<"[ ]";
+                    }
+                    else
+                    {
+                        cout<<"["<<data[i][j]<<"]";
+                    }
                 }
                 cout<<endl;
             }
